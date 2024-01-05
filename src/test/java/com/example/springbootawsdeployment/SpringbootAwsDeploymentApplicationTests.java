2698794534631f8834e846bcacf24eeaa5a86902
Dileep1506 <pmdileepyadav1506@gmail.com>
2024-01-05 18:22:19 +0530

    first commit

---
+++ src/test/java/com/example/springbootawsdeployment/SpringbootAwsDeploymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootawsdeployment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAwsDeploymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
